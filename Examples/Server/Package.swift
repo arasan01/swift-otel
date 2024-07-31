81e4d0c6230673a704a9e148278c89e9f644c19c
arasan01 <developer@arasan01.com>
2024-08-01 02:03:22 +0900

    お試し

--- Examples/Server/Package.swift
+++ Examples/Server/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v14),
     ],
     dependencies: [
-        .package(name: "swift-otel", path: "../.."),
+        .package(url: "https://github.com/slashmo/swift-otel.git", from: "0.9.0"),
         .package(url: "https://github.com/hummingbird-project/hummingbird", exact: "2.0.0-alpha.1"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", from: "2.4.1"),
@@ -16,7 +16,7 @@ let package = Package(
     ],
     targets: [
         .executableTarget(
-            name: "ServerExample",
+            name: "App",
             dependencies: [
                 .product(name: "OTel", package: "swift-otel"),
                 .product(name: "OTLPGRPC", package: "swift-otel"),
@@ -26,5 +26,6 @@ let package = Package(
                 .product(name: "Tracing", package: "swift-distributed-tracing"),
             ]
         ),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
